464e10f3bcebfeb3b046b94ce40a58037163bdf7
Stefan Koch <webokoch@gmx.de>
2021-08-25 15:20:38 +0200

    form refactored

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'planes#index', as: "planes"
-
-
   # get '/planes', to: 'planes#index'
   get '/new', to: 'planes#new', as: "new_plane"
   get '/:id', to: 'planes#show', as: "plane"
