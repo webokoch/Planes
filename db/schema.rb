13a3014ecc066ea176c1688b036c2f31ae768dd4
Stefan Koch <webokoch@gmx.de>
2021-08-25 10:58:15 +0200

    added validations to plane model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_083043) do
+ActiveRecord::Schema.define(version: 2021_08_25_084121) do
 
   create_table "planes", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_08_25_083043) do
     t.integer "passengers"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "manufactor"
   end
 
 end
