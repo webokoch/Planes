6a00c032113029bb6eed0e7babd3b1782e3bbf42
Stefan Koch <webokoch@gmx.de>
2021-08-25 10:31:03 +0200

    plane model added

---
+++ db/migrate/20210825083043_create_planes.rb
@@ -0,0 +1,13 @@
+class CreatePlanes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :planes do |t|
+      t.string :name
+      t.string :type
+      t.integer :engines
+      t.integer :range
+      t.integer :passengers
+
+      t.timestamps
+    end
+  end
+end
