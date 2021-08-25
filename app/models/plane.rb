a489507f571d858c84521466adef3ccb8de6d865
Stefan Koch <webokoch@gmx.de>
2021-08-25 11:15:16 +0200

    routes added

--- app/models/plane.rb
+++ app/models/plane.rb
@@ -1,7 +1,8 @@
 class Plane < ApplicationRecord
   validates :name, presence: true
   validates :manufactor, presence: true
-  validates :type, presence: true, inclusion: { in: %w(Super Heavy Medium Light) }
+  validates :type, presence: true,
+    inclusion: { in: %w(Super Heavy Medium Light) }
   validates :range, presence: true, inclusion: { in: 1..20000 }
   validates :passenger, presence: true
   validates :engines, presence: true, inclusion: { in: 1..4 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/planes', to: 'planes#index'
+  get '/planes/:id', to: 'planes#show'
+  get '/planes/new', to: 'planes#new'
+  post '/planes', to: 'planes#create'
+  get '/planes/:id/edit', to: 'planes#edit'
+  patch '/planes/:id', to: 'planes#update'
+  delete '/planes/:id', to: 'planes#detstroy'
 end
