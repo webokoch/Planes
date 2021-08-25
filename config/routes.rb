a2ff923f2d88f4d28384c8fd611ea068fb92ad8f
Stefan Koch <webokoch@gmx.de>
2021-08-25 11:16:29 +0200

    routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/planes', to: 'planes#index'
-  get '/planes/:id', to: 'planes#show'
   get '/planes/new', to: 'planes#new'
+  get '/planes/:id', to: 'planes#show'
   post '/planes', to: 'planes#create'
   get '/planes/:id/edit', to: 'planes#edit'
   patch '/planes/:id', to: 'planes#update'
